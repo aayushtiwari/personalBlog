7e90111ec24b4668bef1860aca118668b4127f2f
aayushtiwari <arsenicaayush@gmail.com>
2017-07-08 22:08:18 +0530

    updated

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 var express=require('express');
 
-var app
+var app=express();
+var port=process.env.PORT||8888;
+app.listen(port,
+  function(){
+console.log(`server is running at port :${port}`)
+}
+  );
